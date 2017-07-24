8835757eb574dfbfe579abd9037f90ce5cd18b85
ailtonbsj <ailton.ifce@gmail.com>
2017-07-24 16:12:16 -0300

    added webserver sample

---
+++ kurose.webserver/WebServer.java
@@ -0,0 +1,42 @@
+	import java.io.*;
+	import java.net.*;
+	import java.util.*;
+
+	class WebServer {
+		public static void main(String argv[]) throws Exception {
+			ServerSocket listenSocket = new ServerSocket(5000);
+			while(true){
+			String requestMessageLine;
+			String fileName;
+			Socket connectionSocket = listenSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(
+				new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(
+				connectionSocket.getOutputStream());
+			requestMessageLine = inFromClient.readLine();
+			StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
+			if(tokenizedLine.nextToken().equals("GET")){
+				fileName = tokenizedLine.nextToken();
+				if(fileName.startsWith("/") == true)
+					fileName = fileName.substring(1);
+				File file = new File(fileName);
+				int numOfBytes = (int) file.length();
+				FileInputStream inFile = new FileInputStream(fileName);
+				byte[] fileInBytes = new byte[numOfBytes];
+				inFile.read(fileInBytes);
+				outToClient.writeBytes("HTTP/1.1 200 OK\r\n");
+				if(fileName.endsWith(".jpg"))
+					outToClient.writeBytes("Content-Type: image/jpeg\r\n");
+				if(fileName.endsWith(".gif"))
+					outToClient.writeBytes("Content-Type: image/gif\r\n");
+				if(fileName.endsWith(".html"))
+					outToClient.writeBytes("Content-Type: text/html\r\n");
+				outToClient.writeBytes("Content-Length:" + numOfBytes + "\r\n");
+				outToClient.writeBytes("\r\n");
+				outToClient.write(fileInBytes, 0, numOfBytes);
+				System.out.println("File requested: " + fileName);
+				connectionSocket.close();
+				} else System.out.println("Bad Request Message");
+			}
+		}
+	}
